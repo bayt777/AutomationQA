6990ef8b19d5f5426554f56350adafd156c181f9
DmitroBaitaliuk <dmytro.baitaliuk@y-tree.com>
2020-02-18 00:08:22 +0200

    Task_15

---
+++ src/com/company/aqa/module1/Task_16.java
@@ -0,0 +1,16 @@
+package com.company.aqa.module1;
+
+/*Задача 16
+Построить таблицу умножения и выведите в консоль посредством вложенных циклов >:o. (вид должен быть как на школьной тетрадке =) )
+Используйте форматированием строк для красивого вывода.*/
+
+public class Task_16 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int k = 1; k < 10; k++) {
+                System.out.print(k * i + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
