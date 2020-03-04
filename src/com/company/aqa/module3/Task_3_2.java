e7a9849ec8c755ef3bfc0ab46022ffa31502d89d
bayt777 <bayt777@gmail.com>
2020-03-05 00:14:19 +0200

    task 3.2

--- src/com/company/aqa/module3/Task_3_2.java
+++ src/com/company/aqa/module3/Task_3_2.java
@@ -6,6 +6,12 @@ package com.company.aqa.module3;
 public class Task_3_2 {
     public static void main(String[] args) {
         String word = "Перевернуть слова, т.е. последовательность слов остается, но слова написаны задом на перед.";
+        char[] array = word.toCharArray();
+        char[] b = new char[array.length];
+        for (int i = 0; i < array.length; i++) {
+            b[(array.length - 1) - i] = array[i];
+        }
+        System.out.println(b);
     }
 
 }
