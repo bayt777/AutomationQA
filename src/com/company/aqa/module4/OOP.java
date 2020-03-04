d385344a8ba91233cafae504c9b63589de686682
DmitroBaitaliuk <dmytro.baitaliuk@y-tree.com>
2020-03-04 20:27:35 +0200

    Add module_4

---
+++ src/com/company/aqa/module4/OOP.java
@@ -0,0 +1,10 @@
+package com.company.aqa.module4;
+
+public class OOP {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.printf("%s and %s", a, b);
+    }
+}
