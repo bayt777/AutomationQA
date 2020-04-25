05059e92317ff90e7e28f6eed231931d8aaa499f
Dmytro Baitaliuk <dmytro.baitaliuk@y-tree.com>
2020-04-25 23:43:53 +0300

    fix task 1.11

--- src/com/company/aqa/module1/Task_11.java
+++ src/com/company/aqa/module1/Task_11.java
@@ -5,11 +5,17 @@ package com.company.aqa.module1;
 Вводится натуральное число. Найти сумму четных цифр, входящих в его состав.
 тест: при вводе числа "1474" на выходе получим "8", так как четными являются лишь две цифры "4".*/
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task_11 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int num = 1474;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число...");
+        int num = Integer.parseInt(reader.readLine());
         int even;
         int sum = 0;
         while (num != 0) {
