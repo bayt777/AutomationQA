dd33a73602282c884773eef46c719518fb23f79f
Dmytro Baitaliuk <dmytro.baitaliuk@y-tree.com>
2020-04-26 00:28:51 +0300

    fix task 1.16

--- src/com/company/aqa/module1/Task_16.java
+++ src/com/company/aqa/module1/Task_16.java
@@ -9,7 +9,7 @@ public class Task_16 {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             for (int k = 1; k < 10; k++) {
-                System.out.print(k * i + "  ");
+                System.out.printf("%4d", k * i);
             }
             System.out.println();
         }
