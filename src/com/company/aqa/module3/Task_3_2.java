f377e72741fe6ec2fdaeaf6399a69320b26bb4f1
Dmytro Baitaliuk <dmytro.baitaliuk@y-tree.com>
2020-04-26 00:55:30 +0300

    fix task 3.2

--- src/com/company/aqa/module3/Task_3_2.java
+++ src/com/company/aqa/module3/Task_3_2.java
@@ -4,14 +4,20 @@ package com.company.aqa.module3;
 Перевернуть слова, т.е. последовательность слов остается, но слова написаны задом на перед.
 */
 public class Task_3_2 {
-    public static void main(String[] args) {
-        String word = "Перевернуть слова, т.е. последовательность слов остается, но слова написаны задом на перед.";
-        char[] array = word.toCharArray();
-        char[] b = new char[array.length];
-        for (int i = 0; i < array.length; i++) {
-            b[(array.length - 1) - i] = array[i];
-        }
-        System.out.println(b);
-    }
 
+    public static void main(String[] args) {
+        String s = "Перевернуть слова, т.е. последовательность слов остается, но слова написаны задом на перед.";
+        StringBuilder temp = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i))) {
+                temp.append(s.charAt(i));
+            } else {
+                res.append(temp.reverse()).append(s.charAt(i));
+                temp.setLength(0);
+            }
+        }
+        System.out.println(res.toString());
+    }
 }
